9844d9dec09d29fcc9af443d409a7f8548630619
Léo Genot <45610291+leogenot@users.noreply.github.com>
2022-01-18 12:18:10 +0100

    added 128 dataset

--- utils/loaders.py
+++ utils/loaders.py
@@ -1,5 +1,6 @@
 import pickle
 import os
+from turtle import colormode
 
 from tensorflow.keras.datasets import mnist, cifar100, cifar10
 from tensorflow.keras.preprocessing.image import (
@@ -250,6 +251,7 @@ def load_dataset(data_name, image_size, batch_size):
     x_train = data_gen.flow_from_directory(
         data_folder,
         target_size=(image_size, image_size),
+        color_mode = "grayscale",
         batch_size=batch_size,
         shuffle=True,
         class_mode="input",
