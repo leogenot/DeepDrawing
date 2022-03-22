5134037cd38f66a754c3feb46b4aaeb23667d4af
Léo Genot <45610291+leogenot@users.noreply.github.com>
2022-03-22 12:21:10 +0100

    Update loaders.py

--- utils/loaders.py
+++ utils/loaders.py
@@ -1,8 +1,6 @@
 import pickle
 import os
-from turtle import colormode
 
-from tensorflow.keras.datasets import mnist, cifar100, cifar10
 from tensorflow.keras.preprocessing.image import (
     ImageDataGenerator,
     load_img,
@@ -144,102 +142,6 @@ def load_model(model_class, folder):
     return model
 
 
-def load_mnist():
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-    x_train = x_train.astype("float32") / 255.0
-    x_train = x_train.reshape(x_train.shape + (1,))
-    x_test = x_test.astype("float32") / 255.0
-    x_test = x_test.reshape(x_test.shape + (1,))
-
-    return (x_train, y_train), (x_test, y_test)
-
-
-def load_mnist_gan():
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-    x_train = (x_train.astype("float32") - 127.5) / 127.5
-    x_train = x_train.reshape(x_train.shape + (1,))
-    x_test = (x_test.astype("float32") - 127.5) / 127.5
-    x_test = x_test.reshape(x_test.shape + (1,))
-
-    return (x_train, y_train), (x_test, y_test)
-
-
-def load_fashion_mnist(
-    input_rows, input_cols, path="./data/fashion/fashion-mnist_train.csv"
-):
-    # read the csv data
-    df = pd.read_csv(path)
-    # extract the image pixels
-    X_train = df.drop(columns=["label"])
-    X_train = X_train.values
-    X_train = (X_train.astype("float32") - 127.5) / 127.5
-    X_train = X_train.reshape(X_train.shape[0], input_rows, input_cols, 1)
-    # extract the labels
-    y_train = df["label"].values
-
-    return X_train, y_train
-
-
-def load_safari(folder):
-
-    mypath = os.path.join("./data", folder)
-    txt_name_list = []
-    for (dirpath, dirnames, filenames) in walk(mypath):
-        for f in filenames:
-            if f != ".DS_Store":
-                txt_name_list.append(f)
-                break
-
-    slice_train = int(
-        80000 / len(txt_name_list)
-    )  ###Setting value to be 80000 for the final dataset
-    i = 0
-    seed = np.random.randint(1, 10e6)
-
-    for txt_name in txt_name_list:
-        txt_path = os.path.join(mypath, txt_name)
-        x = np.load(txt_path)
-        x = (x.astype("float32") - 127.5) / 127.5
-        # x = x.astype('float32') / 255.0
-
-        x = x.reshape(x.shape[0], 28, 28, 1)
-
-        y = [i] * len(x)
-        np.random.seed(seed)
-        np.random.shuffle(x)
-        np.random.seed(seed)
-        np.random.shuffle(y)
-        x = x[:slice_train]
-        y = y[:slice_train]
-        if i != 0:
-            xtotal = np.concatenate((x, xtotal), axis=0)
-            ytotal = np.concatenate((y, ytotal), axis=0)
-        else:
-            xtotal = x
-            ytotal = y
-        i += 1
-
-    return xtotal, ytotal
-
-
-def load_cifar(label, num):
-    if num == 10:
-        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    else:
-        (x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode="fine")
-
-    train_mask = [y[0] == label for y in y_train]
-    test_mask = [y[0] == label for y in y_test]
-
-    x_data = np.concatenate([x_train[train_mask], x_test[test_mask]])
-    y_data = np.concatenate([y_train[train_mask], y_test[test_mask]])
-
-    x_data = (x_data.astype("float32") - 127.5) / 127.5
-
-    return (x_data, y_data)
-
 
 def load_dataset(data_name, image_size, batch_size):
     data_folder = os.path.join("./data", data_name)
@@ -251,69 +153,12 @@ def load_dataset(data_name, image_size, batch_size):
     x_train = data_gen.flow_from_directory(
         data_folder,
         target_size=(image_size, image_size),
-        color_mode = "grayscale",
         batch_size=batch_size,
         shuffle=True,
         class_mode="input",
         subset="training",
+        #color_mode="grayscale",
     )
     print(image_size)
 
     return x_train
-
-
-def load_music(data_name, filename, n_bars, n_steps_per_bar):
-    file = os.path.join("./data", data_name, filename)
-
-    with np.load(file, encoding="bytes", allow_pickle=True) as f:
-        data = f["train"]
-
-    data_ints = []
-
-    for x in data:
-        counter = 0
-        cont = True
-        while cont:
-            if not np.any(np.isnan(x[counter : (counter + 4)])):
-                cont = False
-            else:
-                counter += 4
-
-        if n_bars * n_steps_per_bar < x.shape[0]:
-            data_ints.append(x[counter : (counter + (n_bars * n_steps_per_bar)), :])
-
-    data_ints = np.array(data_ints)
-
-    n_songs = data_ints.shape[0]
-    n_tracks = data_ints.shape[2]
-
-    data_ints = data_ints.reshape([n_songs, n_bars, n_steps_per_bar, n_tracks])
-
-    max_note = 83
-
-    where_are_NaNs = np.isnan(data_ints)
-    data_ints[where_are_NaNs] = max_note + 1
-    max_note = max_note + 1
-
-    data_ints = data_ints.astype(int)
-
-    num_classes = max_note + 1
-
-    data_binary = np.eye(num_classes)[data_ints]
-    data_binary[data_binary == 0] = -1
-    data_binary = np.delete(data_binary, max_note, -1)
-
-    data_binary = data_binary.transpose([0, 1, 2, 4, 3])
-
-    return data_binary, data_ints, data
-
-
-def preprocess_image(data_name, file, img_nrows, img_ncols):
-
-    image_path = os.path.join("./data", data_name, file)
-
-    img = load_img(image_path, target_size=(img_nrows, img_ncols))
-    img = img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = vgg19.preprocess_input(img)
-    return img
